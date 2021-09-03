9cd91b91951199ff0248c68fcea9d4d1a10fdb92
Michal Radomski <michalra@op.pl>
2021-09-04 00:35:49 +0200

    Showing custom children - done

---
+++ appka_02/src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
+import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45 PM"
+          commentWritten="Nice blog post!"
+          imageSrc={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Alex" timeAgo="Today at 2:00 PM" commentWritten="Very nice" imageSrc={faker.image.avatar()} />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00 PM"
+          commentWritten="Could be..."
+          imageSrc={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
