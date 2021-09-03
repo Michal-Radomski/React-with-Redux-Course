f254b68d0efe3746920574725e52138cee7086e0
Michal Radomski <michalra@op.pl>
2021-09-04 01:54:03 +0200

    Sekcja 3 - done

--- appka_02/src/index.js
+++ appka_02/src/index.js
@@ -11,6 +11,12 @@ if (module.hot) {
 const App = () => {
   return (
     <div className="ui container comments">
+      {/* <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard> */}
       <ApprovalCard>
         <CommentDetail
           author="Sam"
