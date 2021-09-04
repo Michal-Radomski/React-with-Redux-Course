48c19083f4f39dc258a4d17ff5c3280313ee7220
Michal Radomski <michalra@op.pl>
2021-09-04 14:49:37 +0200

    Sekcja 4 done

--- appka_03/src/index.js
+++ appka_03/src/index.js
@@ -6,19 +6,34 @@ if (module.hot) {
   module.hot.accept();
 }
 
-// Create a React Component
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (error) => console.log(error)
-  );
-  return (
-    <div>
-      Hi there!
-      <SeasonDisplay />
-    </div>
-  );
-};
+// -Create a React Component
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (error) => console.log(error)
+//   );
+//   return (
+//     <div>
+//       Latitude:
+//       <SeasonDisplay />
+//     </div>
+//   );
+// };
+
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (error) => console.log(error)
+    );
+    return (
+      <div>
+        Latitude:
+        <SeasonDisplay />
+      </div>
+    );
+  }
+}
 
 // Take the React Component and show it on the show
 ReactDOM.render(<App />, document.getElementById("root"));
