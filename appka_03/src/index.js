6e9ce24f9d93be81383ae27896b5139a0557209e
Michal Radomski <michalra@op.pl>
2021-09-04 18:18:35 +0200

    Section 5 done

--- appka_03/src/index.js
+++ appka_03/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
+// import SeasonDisplay from "./SeasonDisplay";
 
 if (module.hot) {
   module.hot.accept();
 }
 
-// -Create a React Component
+// -Create a React Component (Functional)
 // const App = () => {
 //   window.navigator.geolocation.getCurrentPosition(
 //     (position) => console.log(position),
@@ -20,18 +20,39 @@ if (module.hot) {
 //   );
 // };
 
+// -Create a React Class Component
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    //+ THIS IS THE ONLY TIME we do direct assignment of to this.state
+    this.state = {lat: null, errorMessage: ""};
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (error) => console.log(error)
+      (position) => {
+        // console.log(position);
+        // we called setState!!!
+        this.setState({lat: position.coords.latitude});
+      },
+      //+ DON'T DO IT !!!
+      //+ this.state=position.coords.latitude
+
+      (error) => {
+        console.log(error);
+        this.setState({errorMessage: error.message});
+      }
     );
-    return (
-      <div>
-        Latitude:
-        <SeasonDisplay />
-      </div>
-    );
+  }
+
+  // React says we have to define render function!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    if (!this.state.errorMessage && !this.state.lat) {
+      return <div>Loading...</div>;
+    }
   }
 }
 
