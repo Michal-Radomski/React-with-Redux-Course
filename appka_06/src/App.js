2b95720ab329987d4df82d5b281da8bbd4f0c3db
Michal Radomski <michalra@op.pl>
2021-09-08 01:46:42 +0200

    Accordion done, Search Wiki - initial commit

--- appka_06/src/App.js
+++ appka_06/src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+//+ import Accordion from "./Accordion";
+import Search from "./Search";
+
+const items = [
+  {title: "What is React?", content: "Frontend JS library"},
+  {title: "Why use React?", content: "React is a favorite JS library"},
+  {title: "How do you use React?", content: "By creating components"},
+];
 
 const App = () => {
   return (
     <div>
-      <h1>Widget App</h1>
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 };
