262a81c787dc4cac965c1fe195dba6d7e9dbbe92
Michal Radomski <michalra@op.pl>
2021-09-08 02:34:12 +0200

    Search - commit 1

--- appka_06/src/Search.jsx
+++ appka_06/src/Search.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const Search = () => {
+  const [term, setTerm] = useState("");
+  // React.useEffect(() => console.log(term));
+
+  console.log("every render");
+  useEffect(() => {
+    console.log("scenario 3");
+  }, [term]);
+
   return (
     <div>
       <div className="ui form">
         <div className="field">
           <label>Enter Search Term:</label>
-          <input className="input" />
+          <input value={term} onChange={(event) => setTerm(event.target.value)} className="input" />
         </div>
       </div>
     </div>
