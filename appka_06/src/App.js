a5f2639f3340691760f8f1b19e6a4f90180d292c
Michal Radomski <michalra@op.pl>
2021-09-08 21:33:57 +0200

    Appka_06 - DropDown - initial commit

--- appka_06/src/App.js
+++ appka_06/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 //+ import Accordion from "./Accordion";
-import Search from "./Search";
+//+ import Search from "./Search";
+import DropDown from "./DropDown";
 
-const items = [
-  {title: "What is React?", content: "Frontend JS library"},
-  {title: "Why use React?", content: "React is a favorite JS library"},
-  {title: "How do you use React?", content: "By creating components"},
-];
+//+ const items = [
+//   {title: "What is React?", content: "Frontend JS library"},
+//   {title: "Why use React?", content: "React is a favorite JS library"},
+//   {title: "How do you use React?", content: "By creating components"},
+// ];
 
 const App = () => {
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <DropDown />
     </div>
   );
 };
