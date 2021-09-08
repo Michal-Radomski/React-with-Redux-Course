c1a810df2922e840bce947c2b972abba9db7923b
Michal Radomski <michalra@op.pl>
2021-09-09 00:50:40 +0200

    Appka_06 - Dropdown done

--- appka_06/src/App.js
+++ appka_06/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 //+ import Accordion from "./Accordion";
 //+ import Search from "./Search";
 import DropDown from "./DropDown";
@@ -9,12 +9,30 @@ import DropDown from "./DropDown";
 //   {title: "How do you use React?", content: "By creating components"},
 // ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "Red",
+  },
+  {
+    label: "The Color Green",
+    value: "Green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "Blue",
+  },
+];
+
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
   return (
     <div>
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <DropDown />
+      <button onClick={() => setShowDropdown(!showDropdown)}>Toggle DropDown</button>
+      {showDropdown ? <DropDown options={options} selected={selected} onSelectedChange={setSelected} /> : null}
     </div>
   );
 };
