1e77501fff9697dfdcfcf1b00fee5738775be063
Michal Radomski <michalra@op.pl>
2021-09-08 20:33:16 +0200

    Appka_06 - wiki search done

--- appka_06/src/Search.jsx
+++ appka_06/src/Search.jsx
@@ -1,13 +1,65 @@
 import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
   // React.useEffect(() => console.log(term));
-
-  console.log("every render");
+  const [results, setResults] = useState([]);
+  // console.log(results);
   useEffect(() => {
-    console.log("scenario 3");
-  }, [term]);
+    //- Wariant 1 - rekomendowana przez React
+    const searchWiki = async () => {
+      const {data} = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    if (term && !results.length) {
+      searchWiki();
+    } else {
+      const timeOutID = setTimeout(() => {
+        if (term) {
+          searchWiki();
+        }
+      }, 500);
+
+      return () => {
+        clearTimeout(timeOutID);
+      };
+    }
+
+    //- Wariant 2
+    // (async () => {
+    //   await axios.get("https://api.example.com");
+    // })();
+    //- Wariant 3
+    // axios.get("https://api.example.com").then((response) => {
+    //   console.log(response.data);
+    // });
+  }, [term]); //+ Działenie ESlint'a - tu można to zignorować
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`} target="_blank" rel="noreferrer">
+            Go There
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -17,6 +69,7 @@ const Search = () => {
           <input value={term} onChange={(event) => setTerm(event.target.value)} className="input" />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
