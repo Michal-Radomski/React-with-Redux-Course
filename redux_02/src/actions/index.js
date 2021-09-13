db7c78d8ee81e52036772871a345575eb332a8cc
Michal Radomski <michalra@op.pl>
2021-09-13 22:04:20 +0200

    redux_02 app done

--- redux_02/src/actions/index.js
+++ redux_02/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholder from "../APIs/jsonPlaceholder";
 
 // //- Wersja 1 - podstawowa
@@ -8,6 +9,21 @@ import jsonPlaceholder from "../APIs/jsonPlaceholder";
 //     dispatch({type: "FETCH_POSTS", payload: response});
 //   };
 // };
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  //- Wersja alternatywna poniżej
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  //- Wersja alternatywna - tego co wyżej
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
 
 //- Wersja 1A - równoważna -> skrócona
 export const fetchPosts = () => async (dispatch) => {
@@ -26,3 +42,16 @@ export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({type: "FETCH_USER", payload: response.data});
 };
+
+// //+ WERSJA Z LODASH !!!
+// //- Wersja 1
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUsers(id, dispatch);
+// };
+// //- Wersja 2 -  to samo co wyżej - napisana w jednej linii
+// export const fetchUser = (id) => (dispatch) => _fetchUsers(id, dispatch);
+
+// const _fetchUsers = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({type: "FETCH_USER", payload: response.data});
+// });
