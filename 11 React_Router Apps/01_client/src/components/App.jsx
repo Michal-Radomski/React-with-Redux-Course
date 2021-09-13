66c1734097aabed72af3a0d3db2191657d1135b8
Michal Radomski <michalra@op.pl>
2021-09-14 01:50:16 +0200

    React Router Apps - commit02

---
+++ 11 React_Router Apps/01_client/src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {BrowserRouter, Route} from "react-router-dom";
+
+import StreamCreate from "./streams/StreamCreate";
+import StreamList from "./streams/StreamList";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact={true} component={StreamList} />
+          <Route path="/stream/delete" component={StreamEdit} />
+          <Route path="/stream/new" component={StreamDelete} />
+          <Route path="/stream/edit" component={StreamCreate} />
+          <Route path="/stream/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
