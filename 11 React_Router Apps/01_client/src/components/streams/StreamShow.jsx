8c61bb58b91bdb4fb00750ca352bf0c258331299
Michal Radomski <michalra@op.pl>
2021-09-18 19:13:10 +0200

    Redux-Form was changed to Final Form

--- 11 React_Router Apps/01_client/src/components/streams/StreamShow.jsx
+++ 11 React_Router Apps/01_client/src/components/streams/StreamShow.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 import {connect} from "react-redux";
+import flv from "flv.js";
 import {fetchStream} from "../actions";
 
 class StreamShow extends React.Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.idStream);
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
   }
+
+  componentDidMount() {
+    const {idStream} = this.props.match.params;
+    // console.log(this.videoRef);
+    this.props.fetchStream(idStream);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const {idStream} = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${idStream}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
@@ -19,6 +50,7 @@ class StreamShow extends React.Component {
         {/* //- Wersja oryginalna
         <h1>{this.props.stream.title}</h1>
         <h5>{this.props.stream.description}</h5> */}
+        <video ref={this.videoRef} style={{width: "80%"}} controls={true} />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
