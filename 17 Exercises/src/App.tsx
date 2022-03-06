ab497549e5fd8d5d7c77919014e26a99692d1fc7
Michal Radomski <michalra@op.pl>
2022-03-06 23:55:41 +0100

    Accordion - initial commit2

--- 17 Exercises/src/App.tsx
+++ 17 Exercises/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+
 import "./App.scss";
+import Accordion from "./accordion/Accordion";
 
 function App() {
-  return <React.Fragment>App</React.Fragment>;
+  return (
+    <React.Fragment>
+      <Accordion />
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ 17 Exercises/src/accordion/Accordion.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Accordion = () => {
+  return (
+    <React.Fragment>
+      <h1>Accordion</h1>
+    </React.Fragment>
+  );
+};
+
+export default Accordion;
