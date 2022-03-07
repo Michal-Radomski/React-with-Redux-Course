63cd89aa6674cc4d36f2d0c968accc7a62e10700
Michal Radomski <michalra@op.pl>
2022-03-07 19:37:55 +0100

    Accordion2 - initial commit

---
+++ 17 Additional Exercises/src/accordion2/Main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Main.scss";
+
+const Main = () => {
+  return (
+    <React.Fragment>
+      <section className="accordion2">
+        <div className="container">Header and Accordion here</div>;
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default Main;
