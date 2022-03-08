50d975f09988eec7a21c469eb5b9e93933215a3f
Michal Radomski <michalra@op.pl>
2022-03-08 16:48:09 +0100

    BootstrapMaterial - initial commit2

--- 17 Additional Exercises/src/BootstrapMaterial/Main3.tsx
+++ 17 Additional Exercises/src/BootstrapMaterial/Main3.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Main3 = () => {
+const Main3 = (): JSX.Element => {
   return (
     <React.Fragment>
       <h1>Bootstrap Material Elements</h1>
+      <div className="spinner-border" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
     </React.Fragment>
   );
 };
