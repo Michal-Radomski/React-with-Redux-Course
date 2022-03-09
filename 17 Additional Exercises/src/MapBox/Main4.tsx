eeb1fee69c2eb2bd9b938ed86760b63a2cefd349
Michal Radomski <michalra@op.pl>
2022-03-09 14:41:56 +0100

    MapBox - initial commit2

--- 17 Additional Exercises/src/MapBox/Main4.tsx
+++ 17 Additional Exercises/src/MapBox/Main4.tsx
@@ -1,3 +1,6 @@
+//* Tutorial: https://docs.mapbox.com/help/getting-started
+//- Api Key is not hosted on GitHub.com
+
 import React from "react";
 
 const MapBoxApiKey = process.env.REACT_APP_MapBox_API_KEY;
