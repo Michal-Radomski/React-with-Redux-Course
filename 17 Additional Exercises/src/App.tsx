2780f4af4597953567263988775a5e124771b088
Michal Radomski <michalra@op.pl>
2022-03-09 14:24:36 +0100

    MapBox - initial commit

--- 17 Additional Exercises/src/App.tsx
+++ 17 Additional Exercises/src/App.tsx
@@ -9,9 +9,13 @@ import Main3 from "./BootstrapMaterial/Main3";
 import UseCallback from "./hooks/UseCallback";
 import UseMemo from "./hooks/UseMemo";
 
+import Main4 from "./MapBox/Main4";
+
 function App() {
   return (
     <React.Fragment>
+      <Main4 />
+      <hr />
       <div>
         <UseCallback />
         <hr />
---
+++ 17 Additional Exercises/src/MapBox/Main4.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MapBoxApiKey = process.env.REACT_APP_MapBox_API_KEY;
+console.log("MapBoxApiKey:", MapBoxApiKey);
+
+const Main4 = () => {
+  return <React.Fragment>Main4</React.Fragment>;
+};
+
+export default Main4;
